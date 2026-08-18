b180a35c1d905c41e525284e230e072e7da0225a
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 22:29:59 -0400

    feat: add menu theme properties for improved consistency and clarity

---
+++ src/module/theme/component/menu.ts
@@ -0,0 +1,14 @@
+import { base, gray, background } from '@global/theme/color';
+
+const menu = {
+  'menu.background': background.background_800,
+  'menu.border': base.transparent,
+  'menu.foreground': gray.gray_300,
+  'menu.selectionBackground': background.background_700,
+  'menu.selectionForeground': gray.gray_300,
+  'menu.separatorBackground': background.background_600,
+  'menu.selectionBorder': base.transparent,
+  'menubar.selectionBorder': base.transparent
+};
+
+export { menu };
