baf2dc29f331f96cba8f967a62424997290f6330
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 20:39:40 -0400

    feat: add title bar theme properties for improved UI consistency

---
+++ src/module/theme/component/title_bar.ts
@@ -0,0 +1,12 @@
+import { gray, base, background } from '@global/theme/color';
+
+const titleBar = {
+  'titleBar.background': background.background_900,
+  'titleBar.activeBackground': background.background_900,
+  'titleBar.activeForeground': gray.gray_200,
+  'titleBar.inactiveBackground': background.background_900,
+  'titleBar.inactiveForeground': gray.gray_200,
+  'titleBar.border': base.transparent
+};
+
+export { titleBar };
