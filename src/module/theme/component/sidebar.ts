0de714166434869f8dafa29c57cc154f6ef7f648
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 23:13:20 -0400

    refactor: update sidebar theme properties for improved consistency and clarity

--- src/module/theme/component/sidebar.ts
+++ src/module/theme/component/sidebar.ts
@@ -1,11 +1,23 @@
-import { gray, background } from '../../../global/theme/color';
+import { gray, base, background } from '@global/theme/color';
+import { alpha } from '@global/util/alpha';
 
 const sidebar = {
   'sideBar.background': background.background_900,
-  'sideBar.border': background.background_500,
   'sideBar.foreground': gray.gray_400,
+  'sideBar.border': background.background_700,
+  'sideBarTitle.foreground': gray.gray_400,
   'sideBarSectionHeader.background': background.background_800,
-  'sideBarTitle.foreground': gray.gray_500
+  'sideBarSectionHeader.foreground': gray.gray_200,
+  'sideBar.dropBackground': background.background_600,
+  'sideBarSectionHeader.border': background.background_700,
+
+  'list.inactiveSelectionBackground': alpha(gray.gray_600, 0.4),
+  'list.inactiveSelectionForeground': gray.gray_300,
+  'list.hoverBackground': alpha(gray.gray_700, 0.35),
+  'list.hoverForeground': gray.gray_300,
+  'list.activeSelectionBackground': alpha(gray.gray_600, 0.4),
+  'list.activeSelectionForeground': gray.gray_300,
+  'list.dropBackground': base.transparent
 };
 
 export { sidebar };
