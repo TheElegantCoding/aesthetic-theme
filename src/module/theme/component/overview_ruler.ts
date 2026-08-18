8e879f34ff2e8725a793ae9c13e91dac70d7fab8
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 23:12:53 -0400

    refactor: update overview ruler theme properties for improved consistency and clarity

--- src/module/theme/component/overview_ruler.ts
+++ src/module/theme/component/overview_ruler.ts
@@ -1,20 +1,23 @@
 import {
   red,
   blue,
+  base,
   green,
   purple,
   yellow
-} from '../../../global/theme/color';
-import { alpha } from '../../../global/util/alpha';
+} from '@global/theme/color';
+import { alpha } from '@global/util/alpha';
 
 const overviewRuler = {
-  'editorOverviewRuler.addedForeground': green.green_600,
+  'editorOverviewRuler.addedForeground': green.green_500,
   'editorOverviewRuler.deletedForeground': red.red_900,
-  'editorOverviewRuler.errorForeground': red.red_600,
-  'editorOverviewRuler.findMatchForeground': alpha(blue.blue_600, 0.45),
-  'editorOverviewRuler.infoForeground': purple.purple_600,
-  'editorOverviewRuler.modifiedForeground': yellow.yellow_600,
-  'editorOverviewRuler.warningForeground': yellow.yellow_900
+  'editorOverviewRuler.errorForeground': red.red_500,
+  'editorOverviewRuler.findMatchForeground': alpha(blue.blue_500, 0.45),
+  'editorOverviewRuler.infoForeground': purple.purple_500,
+  'editorOverviewRuler.modifiedForeground': yellow.yellow_500,
+  'editorOverviewRuler.warningForeground': yellow.yellow_900,
+  'editorOverviewRuler.border': base.transparent,
+  'editorOverviewRuler.bracketMatchForeground': blue.blue_400
 };
 
 export { overviewRuler };
