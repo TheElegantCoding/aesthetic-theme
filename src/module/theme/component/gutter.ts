606583448b3c27d19d8d5b72e3f4689c82e4c59e
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 20:40:04 -0400

    refactor: update gutter theme properties for improved consistency and clarity

--- src/module/theme/component/gutter.ts
+++ src/module/theme/component/gutter.ts
@@ -1,17 +1,22 @@
 import {
   red,
-  gray,
   green,
   yellow,
-  background
-} from '../../../global/theme/color';
+  neutral,
+  background,
+  gray
+} from '@global/theme/color';
 
 const gutter = {
-  'editorGutter.addedBackground': green.green_600,
+  'editorGutter.addedBackground': green.green_500,
   'editorGutter.background': background.background_900,
-  'editorGutter.deletedBackground': red.red_600,
-  'editorGutter.foldingControlForeground': gray.gray_400,
-  'editorGutter.modifiedBackground': yellow.yellow_600
+  'editorGutter.deletedBackground': red.red_500,
+  'editorGutter.foldingControlForeground': neutral.neutral_400,
+  'editorGutter.modifiedBackground': yellow.yellow_500,
+  'editorGutter.commentRangeForeground': neutral.neutral_600,
+  'editorGutter.itemHoverBackground': background.background_800,
+  'editorLineNumber.activeForeground': gray.gray_200,
+  'editorLineNumber.foreground': gray.gray_500
 };
 
 export { gutter };
