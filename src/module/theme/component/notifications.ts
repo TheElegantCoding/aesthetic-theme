de88dadc030c60718d2ad20af7e7e8e4c760b838
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 22:30:11 -0400

    refactor: update notification theme properties for improved consistency and clarity

--- src/module/theme/component/notifications.ts
+++ src/module/theme/component/notifications.ts
@@ -4,14 +4,17 @@ import {
   blue,
   yellow,
   background
-} from '../../../global/theme/color';
+} from '@global/theme/color';
 
 const notifications = {
-  'notificationCenter.border': gray.gray_500,
+  'notificationCenter.background': background.background_900,
+  'notificationCenter.border': gray.gray_400,
+  'notificationCenterHeader.border': gray.gray_400,
   'notificationCenterHeader.background': background.background_900,
   'notificationCenterHeader.foreground': gray.gray_400,
+  'notificationLink.foreground': blue.blue_500,
   'notifications.background': background.background_900,
-  'notifications.border': gray.gray_500,
+  'notifications.border': gray.gray_400,
   'notifications.foreground': gray.gray_400,
   'notificationsErrorIcon.foreground': red.red_600,
   'notificationsInfoIcon.foreground': blue.blue_600,
