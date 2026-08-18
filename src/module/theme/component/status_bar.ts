25c1a388b8d38693eb1fdee82ee79857175ff2ff
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 23:13:54 -0400

    refactor: update status bar theme properties for improved consistency and clarity

--- src/module/theme/component/status_bar.ts
+++ src/module/theme/component/status_bar.ts
@@ -1,11 +1,24 @@
-import { gray, blue, green } from '../../../global/theme/color';
+import {
+  red,
+  gray,
+  yellow,
+  background
+} from '@global/theme/color';
 
 const statusBar = {
-  'statusBar.background': blue.blue_700,
+  'statusBar.background': background.background_800,
   'statusBar.foreground': gray.gray_300,
-  'statusBar.noFolderBackground': blue.blue_700,
-  'statusBarItem.remoteBackground': green.green_600,
-  'statusBarItem.remoteForeground': gray.gray_300
+  'statusBar.noFolderBackground': background.background_800,
+  'statusBarItem.remoteBackground': background.background_700,
+  'statusBarItem.remoteForeground': gray.gray_300,
+
+  'statusBar.debuggingBackground': background.background_700,
+  'statusBarItem.activeBackground': background.background_600,
+  'statusBarItem.hoverBackground': background.background_600,
+  'statusBarItem.errorBackground': red.red_900,
+  'statusBarItem.errorForeground': red.red_200,
+  'statusBarItem.warningBackground': yellow.yellow_900,
+  'statusBarItem.warningForeground': yellow.yellow_200
 };
 
 export { statusBar };
