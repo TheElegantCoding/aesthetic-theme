02ccba894ae64b3ce0ac7c8bec51bec15d5b478d
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 22:29:35 -0400

    refactor: update input theme properties for improved consistency and clarity

--- src/module/theme/component/input.ts
+++ src/module/theme/component/input.ts
@@ -1,22 +1,26 @@
 import {
   red,
   gray,
-  base,
   blue,
-  yellow
+  base,
+  yellow,
+  background
 } from '../../../global/theme/color';
 
 const input = {
-  'input.background': base.black,
-  'input.foreground': gray.gray_400,
+  'input.background': background.background_700,
+  'input.foreground': gray.gray_200,
   'input.placeholderForeground': gray.gray_700,
   'inputOption.activeBorder': blue.blue_600,
   'inputValidation.errorBackground': red.red_900,
-  'inputValidation.errorBorder': red.red_600,
+  'inputValidation.errorBorder': base.transparent,
+  'inputValidation.errorForeground': gray.gray_300,
   'inputValidation.infoBackground': blue.blue_900,
-  'inputValidation.infoBorder': blue.blue_600,
+  'inputValidation.infoBorder': base.transparent,
+  'inputValidation.infoForeground': gray.gray_300,
   'inputValidation.warningBackground': yellow.yellow_900,
-  'inputValidation.warningBorder': yellow.yellow_600
+  'inputValidation.warningBorder': base.transparent,
+  'inputValidation.warningForeground': gray.gray_300
 };
 
 export { input };
