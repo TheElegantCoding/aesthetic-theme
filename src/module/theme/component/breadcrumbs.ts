b9ab26868d68f6902516ed301b2371edf45b8ef8
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 20:38:49 -0400

    feat: add breadcrumbs theme properties for navigation clarity

---
+++ src/module/theme/component/breadcrumbs.ts
@@ -0,0 +1,9 @@
+import { neutral, background, gray } from '@global/theme/color';
+
+const breadcrumbs = {
+  'breadcrumb.background': background.background_900,
+  'breadcrumb.focusForeground': gray.gray_100,
+  'breadcrumb.foreground': neutral.neutral_400
+};
+
+export { breadcrumbs };
