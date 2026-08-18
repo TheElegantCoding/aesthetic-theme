e50589c2f1cfdd31ff9fdc70f70d34f93b692027
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 22:29:42 -0400

    refactor: update list theme properties for improved consistency and clarity

--- src/module/theme/component/list.ts
+++ src/module/theme/component/list.ts
@@ -2,24 +2,27 @@ import {
   red,
   gray,
   base,
+  blue,
   background
-} from '../../../global/theme/color';
+} from '@global/theme/color';
 
 const list = {
-  'list.activeSelectionBackground': gray.gray_800,
-  'list.activeSelectionForeground': gray.gray_400,
   'list.dropBackground': base.black,
   'list.focusAndSelectionOutline': base.transparent,
   'list.focusBackground': background.background_700,
-  'list.focusForeground': gray.gray_300,
-  'list.hoverBackground': background.background_700,
-  'list.hoverForeground': gray.gray_300,
-  'list.inactiveSelectionBackground': background.background_700,
-  'list.inactiveSelectionForeground': gray.gray_300,
+  'list.focusForeground': gray.gray_200,
+  'list.focusOutline': base.transparent,
+  'list.inactiveFocusBackground': background.background_800,
 
   'listFilterWidget.noMatchesOutline': red.red_600,
   'listFilterWidget.outline': gray.gray_900,
-  'listFilterWidget.shadow': base.black
+  'listFilterWidget.shadow': base.black,
+  'list.highlightForeground': blue.blue_500,
+  'list.filterWidget.background': background.background_800,
+  'list.filterWidget.outline': gray.gray_400,
+  'list.filterWidget.noMatchesOutline': red.red_600,
+
+  'tree.indentGuidesStroke': gray.gray_700
 };
 
 export { list };
