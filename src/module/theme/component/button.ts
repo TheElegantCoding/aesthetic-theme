e2dd4f21563920f8ba1cca7a5ac33b6f33e43597
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 20:39:01 -0400

    refactor: update button theme properties for consistency and clarity

--- src/module/theme/component/button.ts
+++ src/module/theme/component/button.ts
@@ -1,12 +1,12 @@
-import { gray, blue, background } from '../../../global/theme/color';
+import { blue, gray } from '@global/theme/color';
 
 const button = {
   'button.background': blue.blue_800,
-  'button.foreground': gray.gray_300,
+  'button.foreground': gray.gray_100,
   'button.hoverBackground': blue.blue_700,
-  'button.secondaryBackground': gray.gray_800,
-  'button.secondaryForeground': gray.gray_300,
-  'button.secondaryHoverBackground': background.background_600
+  'button.secondaryBackground': gray.gray_600,
+  'button.secondaryForeground': gray.gray_100,
+  'button.secondaryHoverBackground': gray.gray_500
 };
 
 export { button };
