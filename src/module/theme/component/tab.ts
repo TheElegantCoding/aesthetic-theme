8fd25de03814bebe432ad14971d8b5b158c18275
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 23:14:04 -0400

    refactor: update tab theme properties for improved consistency and clarity

--- src/module/theme/component/tab.ts
+++ src/module/theme/component/tab.ts
@@ -1,15 +1,32 @@
-import { gray, base, background } from '../../../global/theme/color';
-import { alpha } from '../../../global/util/alpha';
+import {
+  base,
+  blue,
+  gray,
+  background
+} from '@global/theme/color';
+import { alpha } from '@global/util/alpha';
 
 const tab = {
-  'editorGroup.dropBackground': alpha(background.background_900, 0.9),
-  'editorGroupHeader.noTabsBackground': background.background_900,
   'editorGroupHeader.tabsBackground': background.background_900,
-  'tab.activeBackground': background.background_900,
-  'tab.activeForeground': gray.gray_300,
+  'editorGroupHeader.tabsBorder': base.transparent,
+  'editorGroupHeader.noTabsBackground': background.background_900,
+  'editorGroup.dropBackground': alpha(background.background_900, 0.9),
+
+  'tab.activeBackground': background.background_700,
+  'tab.activeForeground': gray.gray_100,
+  'tab.activeBorder': blue.blue_600,
+  'tab.activeBorderTop': base.transparent,
+  'tab.activeBorderBottom': blue.blue_600,
+
   'tab.border': base.transparent,
   'tab.inactiveBackground': background.background_900,
-  'tab.inactiveForeground': gray.gray_500
+  'tab.inactiveForeground': gray.gray_400,
+  'tab.hoverBackground': background.background_800,
+  'tab.hoverForeground': gray.gray_200,
+  'tab.unfocusedActiveBackground': background.background_800,
+  'tab.unfocusedActiveForeground': gray.gray_300,
+  'tab.unfocusedInactiveBackground': background.background_900,
+  'tab.unfocusedInactiveForeground': gray.gray_600
 };
 
 export { tab };
