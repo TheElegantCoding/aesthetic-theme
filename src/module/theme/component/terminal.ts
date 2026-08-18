f1e8480379a92b8ed00a5d8e421498d5ceb544f3
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 23:14:22 -0400

    refactor: update terminal theme properties for improved consistency and clarity

--- src/module/theme/component/terminal.ts
+++ src/module/theme/component/terminal.ts
@@ -1,20 +1,13 @@
-import {
-  gray,
-  blue,
-  background
-} from '../../../global/theme/color';
-import { alpha } from '../../../global/util/alpha';
+import { gray, background } from '@global/theme/color';
+import { alpha } from '@global/util/alpha';
 
 const terminal = {
-  'panel.background': background.background_900,
-  'panel.border': gray.gray_500,
-  'panelTitle.activeBorder': gray.gray_500,
-  'panelTitle.activeForeground': gray.gray_400,
-  'panelTitle.inactiveForeground': gray.gray_600,
   'terminal.border': gray.gray_500,
-  'terminal.foreground': gray.gray_300,
-  'terminal.selectionBackground': alpha(blue.blue_700, 0.55),
+  'terminal.background': background.background_900,
+  'terminal.foreground': gray.gray_200,
+  'terminal.selectionBackground': alpha(gray.gray_500, 0.35),
   'terminalCursor.background': gray.gray_300,
+  'terminal.inactiveSelectionBackground': alpha(gray.gray_500, 0.35),
   'terminalCursor.foreground': gray.gray_300
 };
 
