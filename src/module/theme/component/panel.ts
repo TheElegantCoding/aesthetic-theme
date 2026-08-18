0b2aa92204ac6679274f385905d13feeaf4f7d79
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 23:12:59 -0400

    feat: add panel theme properties for improved consistency and clarity

---
+++ src/module/theme/component/panel.ts
@@ -0,0 +1,12 @@
+import { gray, background } from '@global/theme/color';
+
+const panel = {
+  'panel.background': background.background_900,
+  'panel.border': gray.gray_500,
+  'panelTitle.activeBorder': gray.gray_500,
+  'panelTitle.activeForeground': gray.gray_200,
+  'panelTitle.inactiveForeground': gray.gray_600,
+  'panelTitle.inactiveBackground': background.background_800
+};
+
+export { panel };
