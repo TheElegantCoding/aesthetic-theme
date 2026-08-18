cc337d9771f8e94e51fcc70e47a608c8cf7188f6
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-17 20:38:32 -0400

    refactor: update widget theme properties for consistency and clarity

--- src/module/theme/component/widget.ts
+++ src/module/theme/component/widget.ts
@@ -1,16 +1,22 @@
-import { gray, background, base } from '../../../global/theme/color';
+import { gray, base, background } from '../../../global/theme/color';
 
 const widget = {
   'editorHoverWidget.background': background.background_900,
   'editorHoverWidget.border': gray.gray_500,
-  'editorHoverWidget.foreground': gray.gray_300,
+  'editorHoverWidget.foreground': gray.gray_200,
+  'editorHoverWidget.statusBarBackground': background.background_800,
   'editorSuggestWidget.background': background.background_900,
-  'editorSuggestWidget.border': gray.gray_500,
+  'editorSuggestWidget.border': base.transparent,
   'editorSuggestWidget.foreground': gray.gray_400,
-  'editorSuggestWidget.highlightForeground': gray.gray_300,
-  'editorSuggestWidget.selectedBackground': background.background_700,
-  'editorWidget.background': background.background_900,
-  'editorWidget.border': background.background_500,
+  'editorSuggestWidget.highlightForeground': gray.gray_400,
+  'editorSuggestWidget.selectedBackground': background.background_800,
+  'editorSuggestWidget.selectedForeground': gray.gray_200,
+  'editorSuggestWidget.selectedIconForeground': gray.gray_100,
+  'editorSuggestWidget.focusHighlightForeground': gray.gray_100,
+
+  'editorWidget.background': background.background_800,
+  'editorWidget.resizeBorder': gray.gray_500,
+  'editorWidget.border': base.transparent,
   'widget.shadow': base.black
 };
 
