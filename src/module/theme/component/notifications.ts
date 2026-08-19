fe3170cbc4dbbbd73b8dc3c71e794a77a070f78f
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-19 10:10:02 -0400

    refactor: update notification theme properties for improved consistency

--- src/module/theme/component/notifications.ts
+++ src/module/theme/component/notifications.ts
@@ -2,24 +2,25 @@ import {
   red,
   gray,
   blue,
+  base,
   yellow,
   background
 } from '@global/theme/color';
 
 const notifications = {
-  'notificationCenter.background': background.background_900,
-  'notificationCenter.border': gray.gray_400,
+  'notificationCenter.background': background.background_800,
   'notificationCenterHeader.border': gray.gray_400,
-  'notificationCenterHeader.background': background.background_900,
-  'notificationCenterHeader.foreground': gray.gray_400,
+  'notificationCenterHeader.background': background.background_800,
+  'notifications.background': background.background_800,
+  'notificationCenterHeader.foreground': gray.gray_200,
+  'notifications.foreground': gray.gray_300,
+  'notifications.border': base.transparent,
+
+  'notificationsErrorIcon.foreground': red.red_500,
+  'notificationsInfoIcon.foreground': blue.blue_500,
+  'notificationsWarningIcon.foreground': yellow.yellow_500,
   'notificationLink.foreground': blue.blue_500,
-  'notifications.background': background.background_900,
-  'notifications.border': gray.gray_400,
-  'notifications.foreground': gray.gray_400,
-  'notificationsErrorIcon.foreground': red.red_600,
-  'notificationsInfoIcon.foreground': blue.blue_600,
-  'notificationsWarningIcon.foreground': yellow.yellow_600,
-  'notificationToast.border': gray.gray_500
+  'notificationToast.border': base.transparent
 };
 
 export { notifications };
