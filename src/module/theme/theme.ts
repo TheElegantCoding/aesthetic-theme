bb2f4b5d3b338215299ec52548a3bc3458de421a
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-23 19:21:59 -0400

    feat: refactor theme creation to use dynamic color functions and improve structure

--- src/module/theme/theme.ts
+++ src/module/theme/theme.ts
@@ -1,21 +1,26 @@
-import { vscodeStyles } from './component/global';
-import { common } from './syntax/common';
-import { css } from './syntax/css';
-import { jsTs } from './syntax/js_ts';
-import { json } from './syntax/json';
-import { markdown } from './syntax/markdown';
+import { getCommon } from '@module/theme/syntax/common';
+import { getCss } from '@module/theme/syntax/css';
+import { getJsTs } from '@module/theme/syntax/js_ts';
+import { getJson } from '@module/theme/syntax/json';
+import { getMarkdown } from '@module/theme/syntax/markdown';
 
-const theme = {
-  colors: vscodeStyles,
-  name: 'Aesthetic theme',
-  tokenColors: [
-    ...jsTs,
-    ...common,
-    ...css,
-    ...json,
-    ...markdown
-  ],
-  type: 'dark'
+import { getVscodeStyles } from './component/vscode_styles';
+
+import type { ThemeType } from '@global/type/theme_type';
+
+const createTheme = ({ colors, type, name }: ThemeType) => {
+  return {
+    name,
+    type,
+    colors: getVscodeStyles(colors),
+    tokenColors: [
+      ...getJsTs(colors),
+      ...getCommon(colors),
+      ...getCss(colors),
+      ...getJson(colors),
+      ...getMarkdown(colors)
+    ]
+  };
 };
 
-export { theme };
+export { createTheme };
