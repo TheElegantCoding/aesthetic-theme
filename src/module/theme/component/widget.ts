b7f0f5469bf2473278beddbc4b72ef5d951229b2
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-27 23:04:06 -0400

    fix: update editorHoverWidget and editorSuggestWidget background colors for consistency

--- src/module/theme/component/widget.ts
+++ src/module/theme/component/widget.ts
@@ -4,11 +4,11 @@ const getWidget = (colors: Palette) => {
   const { gray, base, background } = colors;
 
   return {
-    'editorHoverWidget.background': background.background_900,
+    'editorHoverWidget.background': background.background_950,
     'editorHoverWidget.border': gray.gray_500,
     'editorHoverWidget.foreground': gray.gray_200,
     'editorHoverWidget.statusBarBackground': background.background_800,
-    'editorSuggestWidget.background': background.background_900,
+    'editorSuggestWidget.background': background.background_950,
     'editorSuggestWidget.border': base.transparent,
     'editorSuggestWidget.foreground': gray.gray_400,
     'editorSuggestWidget.highlightForeground': gray.gray_400,
