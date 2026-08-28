1058da3977e0eae050a5389188882848cb8e2689
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-27 23:04:13 -0400

    fix: update button.foreground color for consistency

--- src/module/theme/component/button.ts
+++ src/module/theme/component/button.ts
@@ -5,7 +5,7 @@ const getButton = (colors: Palette) => {
 
   return {
     'button.background': blue.blue_800,
-    'button.foreground': gray.gray_100,
+    'button.foreground': gray.gray_200,
     'button.hoverBackground': blue.blue_700,
     'button.secondaryBackground': gray.gray_600,
     'button.secondaryForeground': gray.gray_100,
