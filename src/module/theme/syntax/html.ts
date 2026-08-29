58c88d053e1a839bc8c5f821aa562d5537ca041b
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-29 12:15:37 -0400

    fix: update HTML syntax highlighting to use consistent color settings

--- src/module/theme/syntax/html.ts
+++ src/module/theme/syntax/html.ts
@@ -1,15 +1,13 @@
 import type { Palette } from '@global/type/color_scheme_type';
 
 const getHtmlSyntax = (colors: Palette) => {
-  const {
-    amber
-  } = colors;
+  const { syntax } = colors;
 
   return [{
     name: 'HTML deprecated tag',
     scope: 'invalid.deprecated.entity.other.attribute-name.html',
     settings: {
-      foreground: amber.amber_600
+      foreground: syntax.amber
     }
   }];
 };
