01a56cdc470d19c18a58d7163d0f8c038bf74db7
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-29 17:48:54 -0400

    feat: update theme creation to include author, maintainers, and semantic class; fix imports for jsTs and haskell syntax

--- src/module/theme/theme.ts
+++ src/module/theme/theme.ts
@@ -1,19 +1,29 @@
 import { commonSyntax } from '@module/theme/syntax/common_syntax';
 import { cssSyntax } from '@module/theme/syntax/css_syntax';
+import { haskellSyntax } from '@module/theme/syntax/haskel_syntax';
 import { htmlSyntax } from '@module/theme/syntax/html_syntax';
-import { getJsTs } from '@module/theme/syntax/js_ts';
+import { jsTsSyntax } from '@module/theme/syntax/js_ts_syntax';
 import { jsonSyntax } from '@module/theme/syntax/json_syntax';
-import { laravelSyntax } from '@module/theme/syntax/laravel_syntax';
 import { markdownSyntax } from '@module/theme/syntax/markdown_syntax';
 
 import { getVscodeStyles } from './component/vscode_styles';
 
 import type { ThemeType } from '@global/type/theme_type';
 
-const createTheme = ({ colors, type, name }: ThemeType) => {
+const createTheme = ({
+  colors,
+  type,
+  name,
+  author,
+  maintainers,
+  semanticClass
+}: ThemeType) => {
   return {
     name,
     type,
+    semanticClass,
+    author,
+    maintainers,
     colors: getVscodeStyles(colors),
     semanticHighlighting: true,
     semanticTokenColors: {
@@ -30,9 +40,9 @@ const createTheme = ({ colors, type, name }: ThemeType) => {
       ...markdownSyntax(colors),
       ...htmlSyntax(colors),
       ...cssSyntax(colors),
-      ...laravelSyntax(colors),
-      ...jsonSyntax(colors)
-      // ...getJsTs(colors),
+      ...jsonSyntax(colors),
+      ...jsTsSyntax(colors),
+      ...haskellSyntax(colors)
     ]
   };
 };
