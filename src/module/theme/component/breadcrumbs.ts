54b38408cba61c0335bbb8a25e19bc20da9e08cd
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-28 23:36:23 -0400

    fix: update breadcrumb colors for improved consistency and clarity

--- src/module/theme/component/breadcrumbs.ts
+++ src/module/theme/component/breadcrumbs.ts
@@ -1,12 +1,12 @@
 import type { Palette } from '@global/type/color_scheme_type';
 
 const getBreadcrumbs = (colors: Palette) => {
-  const { gray, neutral, background } = colors;
+  const { gray, background } = colors;
 
   return {
     'breadcrumb.background': background.background_950,
-    'breadcrumb.focusForeground': gray.gray_100,
-    'breadcrumb.foreground': neutral.neutral_400
+    'breadcrumb.focusForeground': gray.gray_200,
+    'breadcrumb.foreground': gray.gray_400
   };
 };
 
