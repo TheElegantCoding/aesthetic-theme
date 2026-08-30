eb4b54cfd159d3df5b4890820552c4235d7db798
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-30 15:54:13 -0400

    feat: add environment syntax definitions for variables and operators

--- src/module/theme/syntax/env_syntax.ts
+++ src/module/theme/syntax/env_syntax.ts
@@ -4,7 +4,20 @@ import type { Palette } from '@global/type/color_scheme_type';
 const environmentSyntax = (colors: Palette) => {
   const { gray, syntax } = colors;
 
-  return [];
+  return [{
+    name: 'environment variable',
+    scope: 'variable.other.env',
+    settings: {
+      fontStyle: 'bold',
+      foreground: syntax.blue
+    }
+  }, {
+    name: 'environment keyword operator',
+    scope: 'keyword.operator.assignment.env',
+    settings: {
+      foreground: gray.gray_300
+    }
+  }];
 };
 
 export { environmentSyntax as envSyntax };
