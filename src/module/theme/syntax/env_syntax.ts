26b1eeacce437f27419749b60059a202865a2100
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-29 23:59:22 -0400

    feat: add initial environment syntax module with type definitions

---
+++ src/module/theme/syntax/env_syntax.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line unicorn/name-replacements
+import type { Palette } from '@global/type/color_scheme_type';
+
+const environmentSyntax = (colors: Palette) => {
+  const { gray, syntax } = colors;
+
+  return [];
+};
+
+export { environmentSyntax as envSyntax };
