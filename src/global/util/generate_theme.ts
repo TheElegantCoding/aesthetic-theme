90b76829c3d8a241849d95fce94239d6872b956f
TheElegantCoding <luismonsalve1941@gmail.com>
2026-08-31 13:22:26 -0400

    refactor: restructure theme generation to support multiple themes and improve code clarity

--- src/global/util/generate_theme.ts
+++ src/global/util/generate_theme.ts
@@ -1,22 +1,32 @@
-import { defaultPalette } from '@global/style/color';
+import { darkerPalette, defaultPalette } from '@global/style/color';
 import { createTheme } from '@module/theme/theme';
 import { mkdir, writeFile } from 'node:fs/promises';
 import { resolve, dirname } from 'node:path';
 
-const outputPath = resolve(process.cwd(), 'dist/aesthetic_dark_theme.json');
-
-const buildTheme = async () => {
-  const theme = createTheme({
-    colors: defaultPalette,
-    type: 'dark',
-    name: 'Aesthetic dark',
-    semanticClass: 'aesthetic-dark',
+const buildTheme = () => {
+  const defaultInformation = {
+    type: 'dark' as 'dark' | 'light',
     author: 'Luis M',
     maintainers: ['Luis M <luismonsalve1941@gmail.com>']
-  });
+  };
 
-  await mkdir(dirname(outputPath), { recursive: true });
-  await writeFile(outputPath, JSON.stringify(theme, null, 2), 'utf-8');
+  const themes = [createTheme({
+    ...defaultInformation,
+    colors: defaultPalette,
+    name: 'Aesthetic dark',
+    semanticClass: 'aesthetic-dark'
+  }), createTheme({
+    ...defaultInformation,
+    colors: darkerPalette,
+    name: 'Aesthetic dark grey',
+    semanticClass: 'aesthetic-dark-grey'
+  })];
+
+  themes.forEach(async (theme) => {
+    const themeOutputPath = resolve(process.cwd(), `dist/${theme.semanticClass}.json`);
+    await mkdir(dirname(themeOutputPath), { recursive: true });
+    await writeFile(themeOutputPath, JSON.stringify(theme, null, 2), 'utf-8');
+  });
 };
 
-await buildTheme();
+buildTheme();
